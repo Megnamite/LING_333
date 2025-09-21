c8894c31ace5e25e9f31f20d1de4e839a5dbf747
SebastianoJGV <88995011+SebastianoJGV@users.noreply.github.com>
2025-09-21 22:00:14 +0000

    Add files via upload

---
+++ dataframeIntro.r
@@ -0,0 +1,83 @@
+# Q.1 Display the first 4 rows of the ldt_data using head() function.
+library(tidyverse)
+ldt_data <- read.csv("ldt_data.csv")
+
+print(head(ldt_data[1:4, ]))# Print all columns, rows 1:4
+
+# Q2: Display the last 7 rows of the ldt_data. 
+
+len <- nrow(ldt_data)
+n <- len - 7
+print(len)
+print(ldt_data[n:len, ]) # For some reason ldt_data[len-7:len,] does not work!!! Talk to prof # nolint: line_length_linter.
+
+# Q3: View the summary statistics for the “Freq” column only using summary().
+
+print(summary(ldt_data$Freq))
+# You see minimum, max, mean, median, and quadrants
+
+print("Q4: Create a new column named “Freq_Doubled” that contains twice the values of the “Freq” column.")
+
+freq_double <- c(ldt_data$Freq) * 2 # New vector with doubled values
+doubles_ldt_data <- cbind(ldt_data, "Freq_Doubled" = freqDouble) #cbind() to add a new column
+print(doubles_ldt_data)
+
+print("Q5: Create a for loop to iterate through each element in the “Length” column and print the length of each word.")
+# Q5: Create a for loop to iterate through each element in the “Length” column and print the length of each word.
+
+for (i in seq_len(nrow(ldt_data))){ # For i from 1:nrows
+    print(nchar(ldt_data$Length[i])) # print nchar of the string in position i
+}
+
+print("Q6: Use a for loop to iterate through the “RT_speed” column and print only the “fast” values.")
+# Q6: Use a for loop to iterate through the “RT_speed” column and print only the “fast” values.
+for (i in seq_len(nrow(ldt_data))){ # For i from 1:nrows
+    if (ldt_data$RT_speed[i] == "fast"){
+        print(ldt_data$RT_speed[i])
+    }
+}
+
+print("Q7: Write code for getting the minimum value in the column Mean_RT")
+# Q7: Write code for getting the minimum value in the column Mean_RT
+
+mean_RT <- c(ldt_data$Mean_RT)
+print(ldt_data$Mean_RT[which(mean_RT == min(mean_RT))]) 
+# which(mean_RT == min(mean_RT)) finds the index of the minimum value
+# Print the value at the minimum index of the row
+
+print("Q8: Write a code to check if the minimum value you got is smaller than 600.")
+# Q8: Write a code to check if the minimum value you got is smaller than 600
+
+mean_RT <- c(ldt_data$Mean_RT)
+minimumMean <- ldt_data$Mean_RT[which(mean_RT == min(mean_RT))]
+if (minimumMean > 600) {
+    print("not smaller")
+} else { #HAS TO BE INLINE WITH CLOSING THE IF STATEMENT
+    print("smaller")
+}
+
+
+# Q9:Question:
+# a. Create a new column by multiplying each value in the Length variable by 20 and store it as a column
+# named Length_20
+# b. get the values in the column Length_20 and store it as a vector named Length_20_v
+# c. create another vector Length_20_v_sub containing only the 21st to 30th elements in Length_20_v
+# d. write a for loop that prints the values in Length_20_v_sub, one by one, if they are smaller than 180.
+
+# Q9 A:
+Length_20_v <- c(ldt_data$Length) * 20 # New vector with 20x values
+lenIncLtdData <- cbind(ldt_data, "Length_20" = Length_20_v) #cbind() to add a new column
+print(lenIncLtdData)
+
+# Q9 B: Already done above!
+
+# Q9 C:
+Length_20_v_sub <- c(Length_20_v[21:30])
+print(Length_20_v_sub)
+
+# Q9 D:
+for (i in seq_len(length(Length_20_v_sub))){
+    if (Length_20_v_sub[i]<180){
+        print(Length_20_v_sub[i])
+    }
+}
---
+++ introNotesFile.r
@@ -0,0 +1,141 @@
+# R Notes Document
+# Sebastiano Giannelli Viscardi
+# September 12th
+# LING 333
+
+library(tidyverse)
+
+windows();
+plot(1, 3) # create a plot in a separate window, works much nicer than just plot() # nolint: line_length_linter.
+
+# Use arrow or equals to create variables
+x <- 5
+x = 5
+
+# Use paste if you want to print with numerous fields
+paste(x, "plus 3 equals:", x+3)
+
+# Can also combine strings
+firstName = "Sebastiano"
+lastName = "Giannelli Viscardi"
+fullName = paste0(firstName, " ", lastName) # Need the space otherwise there is none, can also just use paste() instead of paste0() # nolint: line_length_linter.
+# fullName = paste(firstName, lastName) this works without a space in the middle! # nolint
+fullName
+
+# Syntax of reading from files
+df = read_csv(file.choose())
+df
+
+# Creating lists and vectors
+# Vectors contain only 1 datatype, lists can contain variety
+decimalVector = 1.5:6.5 # creates [1.5, 2.5, 3.5, 4.5, 5.5, 6.5] # nolint
+
+numberVector = c(1, 2, 4, 6, 8) #c() used to combine items
+
+exampleList = list(numberVector, decimalVector) #list created with list(), combines datatypes
+
+#length(exampleList) returns 2, not 1
+#exampleList [2] refers to decimalVector, not an error
+
+
+# Intro to filetypes
+b <- TRUE 
+class(b) # "Logical" is the binary logic class, can also be considered 1 for mathematical purposes
+# I.e. TRUE + 1 == 2
+
+a <- 5
+class(a) # "Numerical" is the number data type, including floats and negative numbers
+
+c <- 3L 
+class(c) # Integer is the normal integer data type, can go negative but not float
+
+d <- "Friday"
+class(d) # Character dataset encapsulates both characters and strings
+
+# ****************
+# *DATA STRUCTURE*
+# ****************
+# Four options, Vectors, Lists, Matrices, Arrays, and Data Frames
+# Created using c() function (the concatenate function)
+
+# Vectors only contain the same type
+# Ordered, changeable, Indexed
+monthList <- c('january', 'february', 'march', 'april', 'may', 'june', "etc") #notice I can use both single and double quotes
+numbers <- c(1, 2, 3, 4, 5)
+testvect <- c(monthList, numbers) # Here both variable types within the vector are also vectors, so this actually works!!
+print(testvect)  # See here
+print(monthList) # or paste(monthList)
+
+# Vectors can also have operations run on them
+# e.g.
+numbers <- c(1, 2, 3, 4, 5)
+numbers * 2 # Multiply each element in the vector by 2
+numbers > 3 # Compare each element in the vector with 3
+print(numbers>3) # Keep in mind it returns binary logic values!!! Not the values of the items in the set
+numbersLogic <- c(numbers > 3)
+print(numbers[numbersLogic]) # SEE THIS WORKS, Create a set based on the truth values and access those truth values
+
+test <- c("len", "leng", "lengt", "length")
+test2 <- c("length", "lengt", "leng", "len")
+test2 > test # The comparison seems to be based on string length?
+
+#############
+
+# Lists can contain multiple variable types
+# Ordered, changeable
+# Created using list() rather than c()
+fruitList <- list("Apple", "Banana", 5.5, 10.5)
+paste(fruitList) # Can Not use print!!
+
+############
+
+# Matrix
+# Akin to python dict, except closer to a table with rows and columns
+thismatrix <- matrix(c(1,2,3,4,5,6), nrow = 3, ncol = 2) # 3 rows, 2 columns, 
+print(thismatrix) # Numbers are added to the matrix top down left to right
+# Can also contain chars, or other values, 
+# You can access values with square brackets,
+# Value 1 is the row, value 2 is the column
+print(thismatrix[2, 2])
+
+# Can also access whole rows and columns
+print(thismatrix[,1]) # This accesses all of column 1
+print(thismatrix[1:2,]) # This accessses all of row 1 to row 2
+# Can always use c() to access numerous rows or columns
+
+# Can add additional columns with cbind()
+thismatrix <- matrix(c("apple", "banana", "cherry","orange","grape", "pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
+newmatrix <- cbind(thismatrix, c("strawberry", "blueberry", "raspberry"))
+thismatrix <- cbind(thismatrix, c("strawberry", "blueberry", "raspberry"))
+print(thismatrix) # This works to change the matrix, or you can make a new one
+
+# Add additional rows with rbind()
+thismatrix <- matrix(c("apple", "banana", "cherry", "orange","grape", "pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
+newmatrix <- rbind(thismatrix, c("strawberry", "blueberry", "raspberry"))
+print(newmatrix)
+
+# Use [-c(1), ]
+
+thismatrix <- matrix(c("apple", "banana", "cherry", "orange", "mango", "pineapple"), nrow = 3, ncol =2)
+#Remove the first row and the first column
+thismatrix
+thismatrix <- thismatrix[-c(1), -c(1)]
+thismatrix 
+
+####################
+
+# Accessing Items from data structures
+# Just use [] and a number for exact address
+# Remember 1 counting, not zero counting
+# i.e. vector_name[1] is the first item
+# vector_name [1:3] is first to third item
+# vector_name[c(1, 3)] is only first and third item
+#vector_name[-2] excludes the 2nd item 
+fruit <- c("Apple", "Banana", "Peach", "Strawberry", "Plum")
+print(fruit[2])
+print(fruit[2:5])
+print(fruit[c(2, 5)])
+print(fruit[-3])
+# You won't get out of bounds errors but a vector will just return NA
+# e.g.
+print(fruit[7]) # Returns NA
