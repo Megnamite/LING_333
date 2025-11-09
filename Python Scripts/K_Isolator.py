5b04a326f1d162260f980f45dc7e884710708b2c
Megnamite <88995011+Megnamite@users.noreply.github.com>
2025-11-08 19:07:42 -0800

    Code changed to include only intervocalic /k/

--- Python Scripts/K_Isolator.py
+++ Python Scripts/K_Isolator.py
@@ -7,11 +7,16 @@ csvfilepath = r"D:\\UBC Coding Environment\VIsual Studio\LING_333_Github_Clone\L
 tg = textgrid.TextGrid.fromFile(filepath)
 f = open(csvfilepath, 'w', newline = '')
 
-fieldname = ['phone', 'minTime', 'maxTime'] # Define headers
-dictWriter = csv.DictWriter(f, fieldnames=fieldname) # Initialise object
+fieldname = ['phone', 'minTime', 'maxTime']
+dictWriter = csv.DictWriter(f, fieldnames=fieldname)
 dictWriter.writeheader()
-for x in range(len(tg[3])): # For how many items in phone tier
-    print(x) # Testing sake
-    if tg[3][x].mark == 'k': 
-        dictWriter.writerow({'phone': str(tg[3][x].mark), 'minTime': str(tg[3][x].minTime), 'maxTime': str(tg[3][x].maxTime)}) # Write a dict, each value assigned to a header
+vowels = 'ioaeu'
+
+for x in range(len(tg[3])):
+   # print(x)
+    if tg[3][x].mark == 'k':
+        if str(tg[3][x-1].mark)[0] in vowels:
+            if str(tg[3][x+1].mark)[0] in vowels:
+                print('intervocalic K found')  
+                dictWriter.writerow({'phone': str(tg[3][x].mark), 'minTime': str(tg[3][x].minTime), 'maxTime': str(tg[3][x].maxTime)})
 f.close()
