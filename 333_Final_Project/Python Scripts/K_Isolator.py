3156d89dee69d2c1ce9e9e0c1b875baa701d4ba0
SebastianoJGV <sgiannelli.viscardi@gmail.com>
2025-11-12 18:37:45 -0800

    Added in Tally marks

--- 333_Final_Project/Python Scripts/K_Isolator.py
+++ 333_Final_Project/Python Scripts/K_Isolator.py
@@ -1,31 +1,55 @@
 import textgrid
 import csv
 
-filepath = r"D:\\UBC Coding Environment\VIsual Studio\LING_333_Github_Clone\LING_333\333_Final_Project\SpiCE Textgrids\VM34A_Cantonese_I1_20191028.TextGrid"
-csvfilepath = r"D:\\UBC Coding Environment\VIsual Studio\LING_333_Github_Clone\LING_333\333_Final_Project\SpiCE Textgrids\VM34A.csv"
-tgout = r"D:\\UBC Coding Environment\VIsual Studio\LING_333_Github_Clone\LING_333\333_Final_Project\SpiCE Textgrids\VM34A_intervocalic.Textgrid"
+
+#Original filepath, csv output, and new textgrid output
+filepath = r"D:\\UBC Coding Environment\VIsual Studio\LING_333-1\333_Final_Project\SpiCE Textgrids\VF19A\VF19A_Cantonese_I2_20181114.TextGrid"
+csvfilepath = r"D:\\UBC Coding Environment\VIsual Studio\LING_333-1\333_Final_Project\SpiCE Textgrids\VF19A\VF19A.csv"
+tgout = r"D:\\UBC Coding Environment\VIsual Studio\LING_333-1\333_Final_Project\SpiCE Textgrids\VF19A\VF19A_intervocalic.Textgrid"
 
 tg = textgrid.TextGrid.fromFile(filepath)
-f = open(csvfilepath, 'w', newline = '')
+f = open(csvfilepath, 'w', newline = '')#Open csv file, create new if not available
 
-fieldname = ['phone', 'minTime', 'maxTime']
+fieldname = ['phone', 'minTime', 'maxTime', 'k total', 'kw total', 'g total', 'gw total']
 dictWriter = csv.DictWriter(f, fieldnames=fieldname)
 dictWriter.writeheader()
+#Initialising variables
 vowels = 'ioaeu'
 kept_intervals = []
+targets = ['k', 'g', 'kw', 'gw']
+kTally = 0
+kwTally = 0
+gTally = 0
+gwTally = 0
 
 for x in range(len(tg[3])):
-    iT = textgrid.Interval(tg[3][x].minTime, tg[3][x].maxTime, tg[3][x].mark)
-    if tg[3][x].mark == 'k':
-        if str(tg[3][x-1].mark)[0] in vowels:
-            if str(tg[3][x+1].mark)[0] in vowels:
-                print('intervocalic K found')  
-                kept_intervals.append(textgrid.Interval(tg[3][x].minTime, tg[3][x].maxTime, tg[3][x].mark)) # Add interval with intervocalic K
-                dictWriter.writerow({
-                    'phone': str(tg[3][x].mark), 
-                    'minTime': str(tg[3][x].minTime), 
-                    'maxTime': str(tg[3][x].maxTime)
-                })
+    currentMark = tg[3][x].mark
+    if currentMark in targets:
+        if str(tg[3][x-1].mark) != "" and str(tg[3][x+1].mark) != "": #If the strings arent empty, check if intervocalic
+            if str(tg[3][x-1].mark)[0] in vowels:
+                if str(tg[3][x+1].mark)[0] in vowels:                    
+                    print('intervocalic ' + tg[3][x].mark + ' found')  
+                    kept_intervals.append(textgrid.Interval(tg[3][x].minTime, tg[3][x].maxTime, tg[3][x].mark)) # Add interval with intervocalic target
+                    dictWriter.writerow({ # Save the phone, minTime, and maxTime of the interval
+                        'phone': str(tg[3][x].mark), 
+                        'minTime': str(tg[3][x].minTime), 
+                        'maxTime': str(tg[3][x].maxTime)
+                    })
+                    if currentMark == 'k':
+                       kTally += 1 
+                    if currentMark == 'kw':
+                        kwTally += 1
+                    if currentMark == 'g':
+                        gTally += 1 
+                    if currentMark == 'gw':
+                        gwTally += 1
+                    
+dictWriter.writerow({ # Save tally
+    'k total' : kTally,
+    'kw total' : kwTally,
+    'g total' : gTally,
+    'gw total' : gwTally 
+})
 
 new_tier = textgrid.IntervalTier(name='k isolation', minTime=tg[3].minTime, maxTime=tg[3].maxTime) # Create a new tier just for K
 for inter in kept_intervals: # Loop through adding the old set to this new tier
