85de7e1472f304d279303149b53d04ad34bb7237
SebastianoJGV <sgiannelli.viscardi@gmail.com>
2025-11-18 20:48:10 -0800

    added autoVOT readme instructions and vot marking procedures

---
+++ 333_Final_Project/autoVOT_Work/vot_marking_procedure.md
@@ -0,0 +1,15 @@
+# Procedure for autoVOT Training
+## Setup:
+- Download audio files and paired intervocalic textgrids
+- Mark down in the google sheet what file you are annotating
+- Create a new interval tier called "vot"
+  - Open the textgrids and audio in praat
+  - Go to Tier on the toolbar
+  - Add interval tier
+  - Leave position as it (the new tier will be a the bottom)
+  - Change the name to "vot"
+
+## Marking VOT
+- Add a point to the textgrid at the start of voicing
+- Add a point to the textgrid at the point of closure release
+- add the text "vot" to the created interval
--- ClassWork/dataVisualisation.r
+++ ClassWork/dataVisualisation.r
@@ -1,8 +1,11 @@
+
 library(tidyverse)
 theme_set(theme_bw(base_size = 12)) # Theme for plot consistency (b&w, font 12)
 
-ldt_df <- read.csv("ldt_data.csv")
+ldt_df <- read.csv("D:\\UBC Coding Environment\\VIsual Studio\\LING_333-1\\ClassWork\\ldt_data.csv")
 
-ggplot(ldt_df, aes(x = Mean_RT)) + #initialise plot with data, specifiy mean_rt at x
-geom_histogram(bins = 13, color = 'black', fill = 'lightblue') #create histogram, 13 "bins", black border light blue inside
+#ggplot(ldt_df, aes(x = Mean_RT)) + #initialise plot with data, specifiy mean_rt at x
+#geom_histogram(bins = 13, color = 'black', fill = 'lightblue') #create histogram, 13 "bins", black border light blue inside
 
+ggplot(ldt_df, aes(x = Length, y = Mean_RT)) +
+geom_point() 
