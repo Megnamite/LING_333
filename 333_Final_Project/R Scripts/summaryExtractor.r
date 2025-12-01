53410e66ce4ee8317da9c346ddcddba83b6be024
Megnamite <sgiannelli.viscardi@gmail.com>
2025-12-01 15:51:48 -0800

    Cleaned up file

--- 333_Final_Project/R Scripts/summaryExtractor.r
+++ 333_Final_Project/R Scripts/summaryExtractor.r
@@ -2,13 +2,6 @@ library(tidyverse)
 pathToCSV <- "C:\\Users\\Maria\\Documents\\UNI\\Visual Studio UBC Workspace\\LING333GithubClone\\LING_333\\333_Final_Project\\data\\SpiCE_DrVot_Output_CSV.csv"
 vot_data <- read.csv(pathToCSV)
 
-can_k <- c()
-can_g <- c()
-can_kw <- c()
-can_gw <- c()
-eng_k <- c()
-eng_g <- c()
-
 cleaneddata <- vot_data%>%
     filter(vot_ms >= 10)
 
@@ -23,12 +16,6 @@ can_kw <- cleaneddata$vot_ms[cleaneddata$phone == "kw"]
 can_gw <- cleaneddata$vot_ms[cleaneddata$phone == "gw"]
 eng_k  <- cleaneddata$vot_ms[cleaneddata$phone == "K"]
 eng_g  <- cleaneddata$vot_ms[cleaneddata$phone == "G"]
-
-can_g
-can_kw
-can_gw
-eng_k
-eng_g
 
 write.csv(can_k, file = "can_k.csv", row.names= FALSE)
 write.csv(can_kw, file = "can_kw.csv", row.names= FALSE)
